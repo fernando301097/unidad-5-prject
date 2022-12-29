226b9c747d2782b572879bd2945df0c4339b1cef
fernando301097 <anred123456@gmail.com>
2022-12-29 16:46:31 -0500

    doc:fire:

--- principal/settings.py
+++ principal/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt',
     'rest_framework.authtoken',
+    'drf_yasg',
     
     'users.apps.UsersConfig',
     'pagos.apps.PagosConfig',
--- principal/urls.py
+++ principal/urls.py
@@ -14,11 +14,26 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from rest_framework.permissions import AllowAny
 
 from pagos2.v1 import urls as v1_urls
 from pagos2.v2 import urls as v2_urls
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title = "pagos Api",
+        default_version = "v2",
+        description = "api de pagos",
+        terms_of_service = "https://www.google.com/policies/terms/",
+        contact = openapi.Contact(email="anred123456@gmail.com"),
+        license = openapi.License(name="BSD Licence"),
+        ),
+    public=True,
+    permission_classes=[AllowAny]
+)
 
 
 
@@ -31,4 +46,6 @@ urlpatterns = [
     path('v2/', include(v2_urls)),
 
     path('users/', include('users.urls')),
+
+    re_path(r"^swagger/$", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
 ]
